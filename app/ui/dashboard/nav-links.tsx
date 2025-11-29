cddc946e0c70759582c9537b08c32d637ca6573c
worrzell <aditya11maharana@gmail.com>
2025-11-30 02:29:50 +0530

    test: next js deploy speed

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -42,7 +42,7 @@ export default function NavLinks() {
               },
             )}          >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
+            <p className="hidden md:block">{link.name} test</p>
           </Link>
         );
       })}
