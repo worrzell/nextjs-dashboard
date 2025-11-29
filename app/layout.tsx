951d2b684dc4a4e24e49d78f505e88b55edf40b7
worrzell <aditya11maharana@gmail.com>
2025-11-30 02:21:59 +0530

    chore: bootstrap app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+import '@/app/ui/global.css';
+
+import { inter } from '@/app/ui/fonts';
+import clsx from 'clsx';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={clsx(inter.className, 'antialiased')}>{children}</body>
     </html>
   );
 }
